21d5f0589aa3df74cf6f248ab2acfc4b15ea92a5
mrClapham <info@gcdesign.co.uk>
2016-02-05 16:23:04 +0000

    Getting the WokBook and WorkSheet by name.

--- src/js/index.js
+++ src/js/index.js
@@ -16,9 +16,7 @@ function init(){
 
 
 function initWithOpenFin(){
-    alert("OpenFin is available");
-    // Your OpenFin specific code to go here...
-    //https://www.bitstamp.net/api/transactions/
+    initExcel();
 };
 
 function initNoOpenFin(){
@@ -34,31 +32,99 @@ function createDivCell(data){
     return _div;
 }
 
-function initBitcoin(){
-    console.log("INIT BITCOIN ");
+function initExcel(){
+    Excel = fin.desktop.Excel;
+    Excel.init();
+    Excel.getConnectionStatus(excelCallback);
+    Excel.addEventListener("connected", onExcelConnected);
 
+    Excel.addEventListener("workbookAdded", excelCallback);
+    Excel.addEventListener("workbookClosed", excelCallback);
+    Excel.addEventListener("workbookActivated", function(w){
+        console.log("THERE HAS BEEN A WORKBOOK workbookActivated");
+    });
+}
+getWorkbookByName = function(name) {
+    return new Promise(function (resolve, reject) {
+        fin.desktop.Excel.getWorkbooks(function (workbooks) {
+            var wb = workbooks.filter(function (d, i) {
+                    return d.name === name + ".xlsx"
+                }
+            );
+            wb.length > 0 ? resolve(wb[0]) : reject("no workbook with the name"+name+".")
+        })
+    });
+};
 
-        var bids_placeholder = document.querySelector("#bids_placeholder");
-        var asks_placeholder = document.querySelector("#asks_placeholder");
-        var pusher = new Pusher('de504dc5763aeef9ff52');
-        var order_book_channel = pusher.subscribe('order_book');
-
-        order_book_channel.bind('data', function(data) {
-            console.log(">>>> DATA: ",data);
-            while (bids_placeholder.firstChild) bids_placeholder.removeChild(bids_placeholder.firstChild);
-            while (asks_placeholder.firstChild) asks_placeholder.removeChild(asks_placeholder.firstChild);
-
-            data.bids.filter(function(dd,ii){
-                return dd[1] > 0
-            }).map(function(d,i){
-                bids_placeholder.appendChild( createDivCell(d) );
-            });
-
-            data.asks.filter(function(dd,ii){
-                return dd[1] > 0
-            }).map(function(d,i){
-                asks_placeholder.appendChild( createDivCell(d) );
+function getWorksheetbyName(book, sheet){
+    return new Promise(function(resolve, reject){
+        getWorkbookByName(book).then(function(d,i){
+           var _sheets = d.getWorksheets(function(ws){
+                var _chosenSheet = ws.filter(function(dd,ii){
+                    return dd.name === sheet
+                });
+               console.log("_chosenSheet : ",_chosenSheet)
+               _chosenSheet.length > 0 ? resolve(_chosenSheet[0]) : reject(book+ " has no worksheet called "+ sheet);
             });
 
         });
+    });
+}
+
+//-- Excel utility function
+
+
+// --- Excel Callbacks
+
+onExcelConnected = function(){
+    ///////
+};
+
+onExcelWorkbookAdded = function(){
+    //////
+};
+
+onExcelWorkbookClosed = function(){
+    ////////
+};
+
+
+
+excelCallback = function(e){
+
+    getWorksheetbyName('bitcoin', 'Sheet991').then(function(s){
+        console.log("THE WORKSHEET HAS BEEN FOUND ",s)
+    }).catch(function(err){
+        console.log("ERROR ", err);
+    })
+};
+
+function setSheetData(sheet, data){
+
+}
+
+
+
+function initBitcoin(){
+    var bids_placeholder = document.querySelector("#bids_placeholder");
+    var asks_placeholder = document.querySelector("#asks_placeholder");
+    var pusher = new Pusher('de504dc5763aeef9ff52');
+    var order_book_channel = pusher.subscribe('order_book');
+
+    order_book_channel.bind('data', function(data) {
+        while (bids_placeholder.firstChild) bids_placeholder.removeChild(bids_placeholder.firstChild);
+        while (asks_placeholder.firstChild) asks_placeholder.removeChild(asks_placeholder.firstChild);
+
+        data.bids.filter(function(dd,ii){
+            return dd[1] > 0
+        }).map(function(d,i){
+            bids_placeholder.appendChild( createDivCell(d) );
+        });
+
+        data.asks.filter(function(dd,ii){
+            return dd[1] > 0
+        }).map(function(d,i){
+            asks_placeholder.appendChild( createDivCell(d) );
+        });
+    });
 }
